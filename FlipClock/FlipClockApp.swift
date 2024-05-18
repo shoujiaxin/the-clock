34d1daee8d13cb680f9727190a3e5ff983a36f3f
Jiaxin Shou <shoujiaxin@gmail.com>
2024-05-19 00:11:40 +0800

    feat: add initial project

---
+++ FlipClock/FlipClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockApp.swift
+//  FlipClock
+//
+//  Created by Jiaxin Shou on 2024/5/19.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
