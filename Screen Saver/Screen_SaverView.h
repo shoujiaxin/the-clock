bc087f1049b0d3897ace6e06d9b94dd2a7e08156
Jiaxin Shou <shoujiaxin@gmail.com>
2024-06-13 10:43:45 +0800

    add target

---
+++ Screen Saver/Screen_SaverView.h
@@ -0,0 +1,12 @@
+//
+//  Screen_SaverView.h
+//  Screen Saver
+//
+//  Created by Jiaxin Shou on 2024/6/13.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Screen_SaverView : ScreenSaverView
+
+@end
---
+++ Screen Saver/Screen_SaverView.m
@@ -0,0 +1,51 @@
+//
+//  Screen_SaverView.m
+//  Screen Saver
+//
+//  Created by Jiaxin Shou on 2024/6/13.
+//
+
+#import "Screen_SaverView.h"
+
+@implementation Screen_SaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
